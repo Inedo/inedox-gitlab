abafeeae71ef43dd34c10b146f418775b252c49c
Greg Divis <gdivis@inedo.com>
2024-10-17 20:56:12 -0400

    update to sdk 3.1

--- GitLab/InedoExtension/ListVariableSources/BranchListVariableSource.cs
+++ GitLab/InedoExtension/ListVariableSources/BranchListVariableSource.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Inedo.Documentation;
 using Inedo.ExecutionEngine;
 using Inedo.Extensibility;
@@ -16,65 +12,64 @@ using Inedo.Web;
 using Inedo.Web.Controls;
 using Inedo.Web.Controls.SimpleHtml;
 
-namespace Inedo.Extensions.GitLab.ListVariableSources
+namespace Inedo.Extensions.GitLab.ListVariableSources;
+
+[DisplayName("[Obsolete] GitLab Branches")]
+[Description("Branches from a GitLab repository.")]
+[Undisclosed]
+public sealed class BranchListVariableSource : DynamicListVariableType, IMissingPersistentPropertyHandler
 {
-    [DisplayName("[Obsolete] GitLab Branches")]
-    [Description("Branches from a GitLab repository.")]
-    [Undisclosed]
-    public sealed class BranchListVariableSource : DynamicListVariableType, IMissingPersistentPropertyHandler
+    [Persistent]
+    [ScriptAlias("From")]
+    [DisplayName("From GitHub resource")]
+    [SuggestableValue(typeof(SecureResourceSuggestionProvider<GitLabRepository>))]
+    public string ResourceName { get; set; }
+
+    [Persistent]
+    [ScriptAlias("Project")]
+    [DisplayName("Project name")]
+    [PlaceholderText("Use project from resource")]
+    [SuggestableValue(typeof(ProjectNameSuggestionProvider))]
+    public string ProjectName { get; set; }
+
+    void IMissingPersistentPropertyHandler.OnDeserializedMissingProperties(IReadOnlyDictionary<string, string> missingProperties)
     {
-        [Persistent]
-        [ScriptAlias("From")]
-        [DisplayName("From GitHub resource")]
-        [SuggestableValue(typeof(SecureResourceSuggestionProvider<GitLabRepository>))]
-        public string ResourceName { get; set; }
+        if (string.IsNullOrEmpty(this.ResourceName) && missingProperties.TryGetValue("CredentialName", out var value))
+            this.ResourceName = value;
+    }
 
-        [Persistent]
-        [ScriptAlias("Project")]
-        [DisplayName("Project name")]
-        [PlaceholderText("Use project from resource")]
-        [SuggestableValue(typeof(ProjectNameSuggestionProvider))]
-        public string ProjectName { get; set; }
+    public override async Task<IEnumerable<string>> EnumerateListValuesAsync(VariableTemplateContext context)
+    {
+        var resource = SecureResource.TryCreate(SecureResourceType.GitRepository, this.ResourceName, new ResourceResolutionContext(context.ApplicationId)) as GitLabRepository;
+        if (resource == null)
+            throw new InvalidOperationException($"Could not find resource \"{this.ResourceName}\".");
 
-        void IMissingPersistentPropertyHandler.OnDeserializedMissingProperties(IReadOnlyDictionary<string, string> missingProperties)
+        var client = new GitLabClient(resource, new CredentialResolutionContext(context.ApplicationId));
+
+        var branches = new List<string>();
+        await foreach (var b in client.GetBranchesAsync(resource, CancellationToken.None).ConfigureAwait(false))
+            branches.Add(b.Name);
+
+        return branches;
+    }
+
+    public override ISimpleControl CreateRenderer(RuntimeValue value, VariableTemplateContext context)
+    {
+        if (SecureResource.TryCreate(SecureResourceType.GitRepository, this.ResourceName, new ResourceResolutionContext(context.ApplicationId)) is not GitLabRepository resource 
+            || !Uri.TryCreate(AH.CoalesceString(resource.LegacyApiUrl, GitLabClient.GitLabComUrl).TrimEnd('/'), UriKind.Absolute, out var parsedUri))
+            return new LiteralHtml(value.AsString());
+
+        // Ideally we would use the GitHubClient to retreive the proper URL, but that's resource intensive and we can guess the convention
+        var hostName = parsedUri.Host == "api.gitlab.com" ? "gitlab.com" : parsedUri.Host;
+        return new A($"https://{hostName}/{resource.GroupName}/{resource.ProjectName}/-/tree/{value.AsString()}", value.AsString())
         {
-            if (string.IsNullOrEmpty(this.ResourceName) && missingProperties.TryGetValue("CredentialName", out var value))
-                this.ResourceName = value;
-        }
+            Class = "ci-icon gitlab",
+            Target = "_blank"
+        };
+    }
 
-        public override async Task<IEnumerable<string>> EnumerateListValuesAsync(VariableTemplateContext context)
-        {
-            var resource = SecureResource.TryCreate(SecureResourceType.GitRepository, this.ResourceName, new ResourceResolutionContext(context.ProjectId)) as GitLabRepository;
-            if (resource == null)
-                throw new InvalidOperationException($"Could not find resource \"{this.ResourceName}\".");
-
-            var client = new GitLabClient(resource, new CredentialResolutionContext(context.ProjectId));
-
-            var branches = new List<string>();
-            await foreach (var b in client.GetBranchesAsync(resource, CancellationToken.None).ConfigureAwait(false))
-                branches.Add(b.Name);
-
-            return branches;
-        }
-
-        public override ISimpleControl CreateRenderer(RuntimeValue value, VariableTemplateContext context)
-        {
-            if (SecureResource.TryCreate(SecureResourceType.GitRepository, this.ResourceName, new ResourceResolutionContext(context.ProjectId)) is not GitLabRepository resource 
-                || !Uri.TryCreate(AH.CoalesceString(resource.LegacyApiUrl, GitLabClient.GitLabComUrl).TrimEnd('/'), UriKind.Absolute, out var parsedUri))
-                return new LiteralHtml(value.AsString());
-
-            // Ideally we would use the GitHubClient to retreive the proper URL, but that's resource intensive and we can guess the convention
-            var hostName = parsedUri.Host == "api.gitlab.com" ? "gitlab.com" : parsedUri.Host;
-            return new A($"https://{hostName}/{resource.GroupName}/{resource.ProjectName}/-/tree/{value.AsString()}", value.AsString())
-            {
-                Class = "ci-icon gitlab",
-                Target = "_blank"
-            };
-        }
-
-        public override RichDescription GetDescription()
-        {
-            return new RichDescription("GitLab (", new Hilite(AH.CoalesceString(this.ProjectName, this.ResourceName)), ") branches.");
-        }
+    public override RichDescription GetDescription()
+    {
+        return new RichDescription("GitLab (", new Hilite(AH.CoalesceString(this.ProjectName, this.ResourceName)), ") branches.");
     }
 }
--- GitLab/InedoExtension/VariableTemplates/CommitIdVariable.cs
+++ GitLab/InedoExtension/VariableTemplates/CommitIdVariable.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using Inedo.Documentation;
 using Inedo.ExecutionEngine;
 using Inedo.Extensibility;
-using Inedo.Extensibility.Credentials;
 using Inedo.Extensibility.SecureResources;
 using Inedo.Extensibility.VariableTemplates;
 using Inedo.Extensions.GitLab.Clients;
@@ -13,44 +11,43 @@ using Inedo.Web;
 using Inedo.Web.Controls;
 using Inedo.Web.Controls.SimpleHtml;
 
-namespace Inedo.Extensions.GitLab.VariableTemplates
+namespace Inedo.Extensions.GitLab.VariableTemplates;
+
+[DisplayName("[Obsolete] GitLab CommitId")]
+[Description("CommitId within a GitLab repository.")]
+[Undisclosed]
+public sealed class CommitIdVariable : VariableTemplateType
 {
-    [DisplayName("[Obsolete] GitLab CommitId")]
-    [Description("CommitId within a GitLab repository.")]
-    [Undisclosed]
-    public sealed class CommitIdVariable : VariableTemplateType
+    [Persistent]
+    [DisplayName("From GitHub resource")]
+    [SuggestableValue(typeof(SecureResourceSuggestionProvider<GitLabRepository>))]
+    public string ResourceName { get; set; }
+
+    [Persistent]
+    [ScriptAlias("Repository")]
+    [DisplayName("Repository name")]
+    [PlaceholderText("Use Project Name from GitLab resource")]
+    [SuggestableValue(typeof(ProjectNameSuggestionProvider))]
+    public string ProjectName { get; set; }
+
+    public override ISimpleControl CreateRenderer(RuntimeValue value, VariableTemplateContext context)
     {
-        [Persistent]
-        [DisplayName("From GitHub resource")]
-        [SuggestableValue(typeof(SecureResourceSuggestionProvider<GitLabRepository>))]
-        public string ResourceName { get; set; }
+        if (SecureResource.TryCreate(SecureResourceType.GitRepository, this.ResourceName, new ResourceResolutionContext(context.ApplicationId)) is not GitLabRepository resource 
+            || !Uri.TryCreate(AH.CoalesceString(resource.LegacyApiUrl, GitLabClient.GitLabComUrl).TrimEnd('/'), UriKind.Absolute, out var parsedUri))
+            return new LiteralHtml(value.AsString());
 
-        [Persistent]
-        [ScriptAlias("Repository")]
-        [DisplayName("Repository name")]
-        [PlaceholderText("Use Project Name from GitLab resource")]
-        [SuggestableValue(typeof(ProjectNameSuggestionProvider))]
-        public string ProjectName { get; set; }
-
-        public override ISimpleControl CreateRenderer(RuntimeValue value, VariableTemplateContext context)
+        // Ideally we would use the GitHubClient to retreive the proper URL, but that's resource intensive and we can guess the convention
+        var hostName = parsedUri.Host == "api.gitlab.com" ? "gitlab.com" : parsedUri.Host;
+        return new A($"https://{hostName}/{resource.GroupName}/{AH.CoalesceString(this.ProjectName, resource.ProjectName)}/-/commit/{value.AsString()}", value.AsString()[..7])
         {
-            if (SecureResource.TryCreate(SecureResourceType.GitRepository, this.ResourceName, new ResourceResolutionContext(context.ProjectId)) is not GitLabRepository resource 
-                || !Uri.TryCreate(AH.CoalesceString(resource.LegacyApiUrl, GitLabClient.GitLabComUrl).TrimEnd('/'), UriKind.Absolute, out var parsedUri))
-                return new LiteralHtml(value.AsString());
+            Class = "ci-icon gitlab",
+            Target = "_blank"
+        };
+    }
 
-            // Ideally we would use the GitHubClient to retreive the proper URL, but that's resource intensive and we can guess the convention
-            var hostName = parsedUri.Host == "api.gitlab.com" ? "gitlab.com" : parsedUri.Host;
-            return new A($"https://{hostName}/{resource.GroupName}/{AH.CoalesceString(this.ProjectName, resource.ProjectName)}/-/commit/{value.AsString()}", value.AsString()[..7])
-            {
-                Class = "ci-icon gitlab",
-                Target = "_blank"
-            };
-        }
-
-        public override RichDescription GetDescription()
-        {
-            var repoName = AH.CoalesceString(this.ResourceName, this.ProjectName);
-            return new RichDescription("GitLab (", new Hilite(repoName), ") commit.");
-        }
+    public override RichDescription GetDescription()
+    {
+        var repoName = AH.CoalesceString(this.ResourceName, this.ProjectName);
+        return new RichDescription("GitLab (", new Hilite(repoName), ") commit.");
     }
 }
